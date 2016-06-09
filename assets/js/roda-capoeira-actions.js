6f4df962a69c1985a19e80a2417097fc345ea0b8
unknown <wayfairers.co@gmail.com>
2016-06-10 06:38:27 +1000

    image management

--- assets/js/roda-capoeira-actions.js
+++ assets/js/roda-capoeira-actions.js
@@ -1,9 +1,21 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-function prepareEventHandlers() {
 
-    $('.more-info').click(function() {
+// TODO fix issue with event not loaded when generating dynamic content
+// http://stackoverflow.com/questions/15090942/jquery-event-handler-not-working-on-dynamic-content
+
+function prepareEventHandlers() {
+     // Bind event to static content
+     /*  $('.more-info').click(function() {
+                    var elementA = $(this).parentsUntil('.row.article').last().parent();
+                    console.log(elementA.html());
+                    var moreInfoClicker =  elementA.find('.detail-section')
+                    loadJsonData(moreInfoClicker)
+                    moreInfoClicker.toggle();
+       });*/
+     // Bind event to dynamically generated content
+      $(document.body).on('click','.more-info',function() {
                     var elementA = $(this).parentsUntil('.row.article').last().parent();
                     console.log(elementA.html());
                     var moreInfoClicker =  elementA.find('.detail-section')
@@ -14,52 +26,39 @@ function prepareEventHandlers() {
 /* a specific details for a roda */
 function initLatestRoda() {
      var articlesContainer = $(document).find('.col-md-12.articles');
-     var latestArticle = null;
-     console.log("Step1 ");
-     console.log(""+articlesContainer.html());
-     //Iterate through the json collection
-     //Load template
-     //Append template  as the latest element of the content section
-     //Map current Json data with $Jquery node
+     var latestArticle =  $('.row.article:last');
+     var template;
+   
+     //Load template in local variable template must be on local 
+     //Ajax policy wil prenvent template file to be loaded from other domaina
      $.ajax({
-                    url: 'assets/data/latest.json',
-                    type: 'get',
-                    dataType : 'json',
-                    success: function(data) {
-                        console.log("Step 2 ");
-                          $.each(data, function(key, item) {
-                             console.log("Step 3 ");
-//                             if (latestArticle === null){
-//                             latestArticle = $(articlesContainer).filter('.row.article:last');
-                             $(".col-md-12.articles").load("article.html .row.article",
-                                                       function(responseTxt, statusTxt, xhr){
-                                                        if(statusTxt == "success") {
-                                                            console.log("First article");
-                                                            console.log(articlesContainer.html());
-                                                            var latestArticle = $(articlesContainer).filter('.row.article:last');
-                                                            // Append the text value of the article in the section
-                                                            $(latestArticle).find('.detail-section').text(item.Description);
-                                                        }
-                                                        if(statusTxt == "error")
-                                                        alert("Error: " + xhr.status + ": " + xhr.statusText);
-                            });
-//                            } else {
-//
-//                                //Load the other elements with the template
-//                            }
-
-
-                               // Go the latest article
-                            //$(articlesContainer).load("article.html .row.article");
-
-
-                    });
-                    },
-                    error: function(e) {
-                        console.log('error message'+e.message);
+      url: 'article.html',
+      type: 'get',
+      success: function(data) {
+        template = data;
+      }
+     });
+    
+     $.ajax({
+      url: 'assets/data/latest.json',
+      type: 'get',
+      dataType : 'json',
+      success: function(data) {
+      //Append template  and bind json data
+       $.each(data, function(key, item) {
+        articlesContainer.append(template);
+        latestArticle =  $('.row.article:last');
+        latestArticle.find('.Title').text(item.Title);
+        latestArticle.find('.detail-section').text(item.Description);
+        latestArticle.find('.location').text(item.Location);
+        latestArticle.find('date').text(item.Date);
+        });
+            },
+            error: function(e) {
+                console.log('error message'+e.message);
                         /*alert('Error');*/
-                    }
-             });
+            }
+    });
 }
 function fetchLatestRoda() {
      $.ajax({
@@ -76,15 +75,7 @@ function fetchLatestRoda() {
                         /*alert('Error');*/
                     }
              });
-
-
-    //Iterate through the json collection
-    //Load template
-    //Append template  as the latest element of the content section
-    //Map current Json data with $Jquery node
-    //
 }
-
 /* Load  a specific details for a roda */
 function loadJsonData(moreInfoElement) {
             $.ajax({
@@ -105,6 +96,4 @@ function loadJsonData(moreInfoElement) {
 $(document).ready(function () {
     prepareEventHandlers();
     initLatestRoda();
-
-
 });
